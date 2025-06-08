82315dd8f9b4153aac7f9bace437f10e332e3458
Kevin Muraya Njoroge <kenjoroge@icealion.com>
2025-06-08 19:56:36 +0300

    Final touches

--- function.js
+++ function.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.querySelector('.nav-toggle-btn');
+const navLinks = document.querySelectorAll('.nav-links');
 
+toggleBtn.addEventListener('click', () => {
+  navLinks.forEach(link => link.classList.toggle('show-nav'));
+});
